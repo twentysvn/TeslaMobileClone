519bdf800ace9afdd1a83ec106650e371f07ee23
twentysvn <qodim.is30@gmail.com>
2020-12-21 21:26:13 +0700

    fixing

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: "100%",
-    resizeMode: "cover",
+    resizeMode: "contain",
     position: "absolute",
   },
   buttonContainer: {
